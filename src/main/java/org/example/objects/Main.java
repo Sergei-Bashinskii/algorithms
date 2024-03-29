f91a45f1fbc1e2746ac1553f00c38d6992243f13
Sergei-Bashinskii <sergei13121390@gmail.com>
2024-03-29 22:17:00 +0300

    Task

---
+++ src/main/java/org/example/objects/Main.java
@@ -0,0 +1,6 @@
+package org.example.objects;
+
+public class Main {
+    public static void main(String[] args) {
+    }
+}
---
+++ src/main/java/org/example/objects/Model/StringList.java
@@ -0,0 +1,9 @@
+package org.example.objects.Model;
+
+import org.example.objects.Interface.StringListInterface;
+
+//public class StringList implements StringListInterface {
+
+    //public String add(String item);
+
+//}
